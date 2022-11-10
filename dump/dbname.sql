ef8cc6c87cd16fd5919d20deda26f807c8ced2bc
*Mario <*mariojra6@gmail.com*>
2022-11-10 09:32:30 +0100

    Archivos actualizados

--- dump/dbname.sql
+++ dump/dbname.sql
@@ -1,4 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -14,7 +13,7 @@ CREATE TABLE `user_futbol` (
   `equipo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `user_futbol` (`id`, `name`, `surname`, `edad`, `equipo`) VALUES (1,'Fran', 'García Pozuelo', 20, 'Sevilla'), (2,'Manuel', 'Moya Vadillo', 19, 'Real Madrid');
+INSERT INTO `user_futbol` (`id`, `name`, `surname`, `edad`, `equipo`) VALUES (0,'Fran', 'García Pozuelo', 20, 'Sevilla'), (0,'Manuel', 'Moya Vadillo', 19, 'Real Madrid');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
